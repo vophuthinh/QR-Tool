5753bf108baf4c770935643abe1402b33d2c3e28
vophuthinh <vophuthinhcm@gmail.com>
2025-12-11 14:52:04 +0700

    Add server and preview configuration to Vite setup; set port and host for local development.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    server: {
+        port: 3111,
+        host: true,
+    },
+    preview: {
+        port: 3111,
+        host: true,
+    },
     test: {
         environment: 'jsdom',
     },
